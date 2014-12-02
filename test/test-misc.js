df2769240102a16736d5b4240e1510aad8550787
Joshua Bell <inexorabletash@gmail.com>
2014-12-01 21:36:10 -0800

    Resurrect GBK, merge with GB18030

--- test/test-misc.js
+++ test/test-misc.js
@@ -196,7 +196,7 @@ test(
       "macintosh", "windows-874", "windows-1250", "windows-1251",
       "windows-1252", "windows-1253", "windows-1254", "windows-1255",
       "windows-1256", "windows-1257", "windows-1258", "x-mac-cyrillic",
-      "gb18030", "big5", "euc-jp", "iso-2022-jp", "shift_jis",
+      "gbk", "gb18030", "big5", "euc-jp", "iso-2022-jp", "shift_jis",
       "euc-kr"];
 
     encodings.forEach(function (encoding) {
@@ -237,10 +237,17 @@ test(
 
 test(
   function () {
-
     var utf_encodings = ["utf-8", "utf-16le", "utf-16be"];
 
-    var legacy_encodings = ["ibm866", "iso-8859-2", "iso-8859-3", "iso-8859-4", "iso-8859-5", "iso-8859-6", "iso-8859-7", "iso-8859-8", "iso-8859-10", "iso-8859-13", "iso-8859-14", "iso-8859-15", "iso-8859-16", "koi8-r", "koi8-u", "macintosh", "windows-874", "windows-1250", "windows-1251", "windows-1252", "windows-1253", "windows-1254", "windows-1255", "windows-1256", "windows-1257", "windows-1258", "x-mac-cyrillic", "gb18030", "big5", "euc-jp", "iso-2022-jp", "shift_jis", "euc-kr"];
+    var legacy_encodings = [
+      "ibm866", "iso-8859-2", "iso-8859-3", "iso-8859-4", "iso-8859-5",
+      "iso-8859-6", "iso-8859-7", "iso-8859-8", "iso-8859-10",
+      "iso-8859-13", "iso-8859-14", "iso-8859-15", "iso-8859-16", "koi8-r",
+      "koi8-u", "macintosh", "windows-874", "windows-1250", "windows-1251",
+      "windows-1252", "windows-1253", "windows-1254", "windows-1255",
+      "windows-1256", "windows-1257", "windows-1258", "x-mac-cyrillic",
+      "gbk", "gb18030", "big5", "euc-jp", "iso-2022-jp", "shift_jis",
+      "euc-kr"];
 
     utf_encodings.forEach(function(encoding) {
       assert_equals(new TextDecoder(encoding).encoding, encoding);
